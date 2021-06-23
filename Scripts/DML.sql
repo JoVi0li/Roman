ee6e1280196ab0c54a43915bb3388cdd8616574f
João Vitor <jovi.oli04@gmail.com>
2021-06-23 18:13:44 -0300

    Scripts DDL e DML alterados

--- Scripts/DML.sql
+++ Scripts/DML.sql
@@ -7,32 +7,31 @@ VALUES					('Administrador')
 						,('Professor');
 GO
 
-INSERT INTO Usuario  ( IdTipoUsuario , NomeUsuario  , Email   ,			Senha )
-VALUES				 (  1			,'Caique'	,'Caique@adm.com'		,1234)
-					,(	1			,'saulo'	,'Saulo@adm.com	'		,1234)
-					,(	2			,'Aparecido','Aparecido@email.com'	,1234)
-					,(	2			,'Carlos'	,'Carlos@email.com'		,1234)
-					,(	2			,'João'		,'Joao@email.com'		,1234)
-					,(	2			,'Joyce'	,'Joyce@email.com'		,1234)
-					,(	2			,'Yuri'		,'Yuri@email.com'		,1234);
+INSERT INTO Usuario  ( IdTipoUsuario , NomeUsuario  , Email   ,			Senha, IdEquipe )
+VALUES				 (  1			,'Caique'	,'Caique@adm.com'		,1234, 1)
+					,(	1			,'saulo'	,'Saulo@adm.com	'		,1234, 1)
+					,(	2			,'Aparecido','Aparecido@email.com'	,1234, 1)
+					,(	2			,'Carlos'	,'Carlos@email.com'		,1234, 2)
+					,(	2			,'João'		,'Joao@email.com'		,1234, 2)
+					,(	2			,'Joyce'	,'Joyce@email.com'		,1234, 3)
+					,(	2			,'Yuri'		,'Yuri@email.com'		,1234, 3);
 GO
 
 
-INSERT INTO Equipe (IdUsuario	,NomeEquipe)
-VALUES				(3			,'Desenvolvimento')
-					,(4			,'Redes'		  )
-					,(5			,'Multimídia'	  );
+INSERT INTO Equipe (NomeEquipe)
+VALUES				('Desenvolvimento')
+					,('Redes'		  )
+					,('Multimídia'	  );
 GO
 
-INSERT INTO Projeto (IdTipoUsuario,	NomeProjeto)
-VALUES				( 2,		'Controle de Estoque');
+INSERT INTO Projeto (IdUsuario,	NomeProjeto, Idtema)
+VALUES				( 2,		'Controle de Estoque', 1);
 
 GO
 
-INSERT INTO Tema (IdProjeto,  NomeTema				,Descricao	,			  Ativo)
-VALUES			( 1,		  'Gestão',	  'Programa para gerir um projeto',		1)
-				,(1,		  'HQs',	  'Progrma de História em Quadrinhos',  1);
-
+INSERT INTO Tema ( NomeTema				,Descricao	,			  Ativo)
+VALUES			('Gestão',	  'Programa para gerir um projeto',		1)
+				,('HQs',	  'Progrma de História em Quadrinhos',  1);
 GO
 
 
